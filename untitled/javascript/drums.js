79cb2d12356c5c9fe74a43ea72e90b8da1e66e81
Marc Freeman <freeman.marc880@gmail.com>
2017-02-14 14:53:37 +0000

    Update drums.js

--- untitled/javascript/drums.js
+++ untitled/javascript/drums.js
@@ -1,17 +1,34 @@
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // ES6 string
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //ES6 string
-    if(!audio) return; //stop function running all together
+    if (audio != audio) {
+        return;
+    } //stop function running all together
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
 }
 
 function removeTransition(e) {
-    if(e.propertyName !== 'transform') return; //skip if it's not a transform
+    if (e.propertyName !== 'transform') return; //skip if it's not a transform
     this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
+
+/* You can use either the for each or the for loop, they both work the same as
+** the loop below
+
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+keys.forEach(function (key) {
+    key.addEventListener('transitionend', removeTransition);
+});
+*/
+
+for (var i = 0; i < keys.length; i++) {
+    keys[i].addEventListener('transitionend', removeTransition);
+}
+
+
 window.addEventListener('keydown', playSound);
